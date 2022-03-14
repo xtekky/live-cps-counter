cc0ad1bed74cad95ed18c4ff8765069a0a93f0f3
xtekky <98614666+xtekky@users.noreply.github.com>
2022-03-14 20:04:39 +0100

    Update live-cps.py

--- live-cps.py
+++ live-cps.py
@@ -2,36 +2,42 @@ from pynput.mouse import Listener
 from datetime import datetime, timedelta
 import time
 
-#assigning variables for click and cps time length
 raw_clicks = 0
-sec = 2
+sec = 0.8
 print('Click to start')
 
 
-#function to start code by a click
 def on_click(x, y, button, pressed):
     global start
-    listener.stop() #stopping 1st listener
-    start = time.time() #starting timer
-    print('- started time -')
-with Listener(on_click=on_click) as listener: 
+    listener.stop()
+    start = time.time()
+    print('')
+
+with Listener(on_click=on_click) as listener:
     listener.join()
 
-
-#looping the cps_counter function
 while True:
-    def cps_counter(x, y, button, pressed):
+    def on_click(x, y, button, pressed):
             global raw_clicks
             global sec
             global start
-            raw_clicks = raw_clicks + 1 #every click, add 1 to raw click
-            if time.time() - start > sec: #make script stop after 'sec' seconds to get a new value
-                listener.stop() #stopoping listener
-                raw_raw = (raw_clicks / 2) #dividing raw clicks because pynput counts clickand release so the clicks are double
+            raw_clicks = raw_clicks + 1
+            if time.time() - start > sec:
+                listener.stop()
+                raw_raw = (raw_clicks / 2)
                 print("\r", end="")
-                print(raw_raw/sec, 'cps', end="") #printing cps
-                raw_clicks = 0 #resetting click count
-                start = time.time() #restarting timer
+                print(raw_raw/sec, 'cps', end="")
+                raw_clicks = 0
+                start = time.time()
+
+
+
     with Listener(on_click=on_click) as listener:
         listener.join()
+
+
+
+
+
+
 
